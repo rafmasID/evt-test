3934d7ca0851071e35eb9ec8e376fbf1d7080ad8
rafmasID <rafmasID@gmail.com>
2013-11-10 20:56:20 +0100

    primo

--- js/main/fullscreen_request.js
+++ js/main/fullscreen_request.js
@@ -1,14 +1,6 @@
 /**
  * 
- * Full screen request
- * Version 0.1 (201210)
- *
- * Dual licensed under the MIT and GPL licenses:
- * http://www.opensource.org/licenses/mit-license.php
- * http://www.gnu.org/licenses/gpl.html
- * e ci risiamoooooooooos ahahahah 
- * @author RafMas questo qui! --- terza modifica opz ---
- * @since 2012 cerchiamo le differenze!!!!
+** ecco il primo
  *
  **/
 
